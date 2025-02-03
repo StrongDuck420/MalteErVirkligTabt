e0009eb2ea077982c611ee0f2c2fe17e8fbe7b1a
Sand6968 <Sanderholmca@gmail.com>
2025-02-03 12:04:35 +0100

    UI

---
+++ ui.gd
@@ -0,0 +1,24 @@
+extends Control
+
+
+
+
+
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://Main.tscn")
+	
+	
+	
+
+
+func _on_tips_pressed() -> void:
+	get_tree().change_scene_to_file("res://Tips UI.tscn")
+	
+	
+	
+
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
